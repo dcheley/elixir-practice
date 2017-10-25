ff14f49e4eb3e706e24dc2ed354ff5eaafe3bc58
dcheley <danielcheley@hotmail.com>
2017-10-25 14:29:12 -0400

    Adding quote:& unquote: examples

--- simple.exs
+++ simple.exs
@@ -16,3 +16,10 @@ IO.puts subtract.(1, 2)
 
 # Not the same, '' is charlist while "" is string
 IO.puts 'hello' == "hello"
+
+number = 5
+# Must use unquote fn to inject value into placeholder when calling fn's inner representation (using quote)
+IO.puts Macro.to_string(quote do: 10 + unquote(number))
+
+middle = [3, 4, 5]
+IO.puts Macro.to_string(quote do: [1, 2, unquote_splicing(middle), 6])
