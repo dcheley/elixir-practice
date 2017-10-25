71c854e2c41057d0d18cea965937f33096afc3d2
dcheley <danielcheley@hotmail.com>
2017-10-25 15:53:01 -0400

    Adding callback to Bucket test

--- kv/test/bucket_test.exs
+++ kv/test/bucket_test.exs
@@ -2,8 +2,13 @@ defmodule KV.BucketTest do
   # Only use async: true if case doesn't rely on change or global values
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
-    {:ok, bucket} = start_supervised KV.Bucket
+  setup do
+    {:ok, bucket} = start_supervised(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+     # `bucket` is now the bucket from the setup block
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
